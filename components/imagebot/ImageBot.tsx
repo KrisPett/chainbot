0a890e746e00b9d884827f8ade34c3c22d134f31
KQT3 <70701718+KQT3@users.noreply.github.com>
2023-03-13 19:39:36 +0100

    commit

--- components/imagebot/ImageBot.tsx
+++ components/imagebot/ImageBot.tsx
@@ -26,174 +26,173 @@ type AIPromptRequestBody = {
   text: string
 }
 
-const fetchDownloadMaterial = () => {
+const fetchDownloadMaterial = (url: string) => {
   const downloadLink = document.getElementById('download-link');
-
+  console.log(url)
 // Trigger click event on the download link to download the image
   if (downloadLink)
     downloadLink.click();
-    // const filename = "https://oaidalleapiprodscus.blob.core.windows.net/private/org-Gj9qRtTFTcQvSvQMlsTGqRzb/user-WIsWrURgDHs7MJkppHrsBBgZ/img-JBl5E0HKm2Uv8CHwwnz3rT09.png?st=2023-03-13T16%3A33%3A48Z&se=2023-03-13T18%3A33%3A48Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-03-13T08%3A00%3A59Z&ske=2023-03-14T08%3A00%3A59Z&sks=b&skv=2021-08-06&sig=YsjB8sz7KxvTlhBAWF97U0B7RE1XzqMNgzNpe0lx2X4%3D"
-    // const extractFilename = filename.split('/').pop();
-    // if (!extractFilename) throw new Error("Filename is undefined");
-    // return fetch(`${filename}`, {method: "GET"})
-    //   .then(res => res.blob())
-    //   .then(blob => {
-    //     const url = window.URL.createObjectURL(blob)
-    //     const a = document.createElement("a")
-    //     a.href = url
-    //     a.download = extractFilename
-    //     document.body.appendChild(a)
-    //     a.click()
-    //     a.remove()
-    //   })
-    //   .catch(err => {
-    //     console.error("err: ", err);
-    //   })
+  // const filename = "https://oaidalleapiprodscus.blob.core.windows.net/private/org-Gj9qRtTFTcQvSvQMlsTGqRzb/user-WIsWrURgDHs7MJkppHrsBBgZ/img-JBl5E0HKm2Uv8CHwwnz3rT09.png?st=2023-03-13T16%3A33%3A48Z&se=2023-03-13T18%3A33%3A48Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-03-13T08%3A00%3A59Z&ske=2023-03-14T08%3A00%3A59Z&sks=b&skv=2021-08-06&sig=YsjB8sz7KxvTlhBAWF97U0B7RE1XzqMNgzNpe0lx2X4%3D"
+  // const extractFilename = filename.split('/').pop();
+  // if (!extractFilename) throw new Error("Filename is undefined");
+  // return fetch(`${filename}`, {method: "GET"})
+  //   .then(res => res.blob())
+  //   .then(blob => {
+  //     const url = window.URL.createObjectURL(blob)
+  //     const a = document.createElement("a")
+  //     a.href = url
+  //     a.download = extractFilename
+  //     document.body.appendChild(a)
+  //     a.click()
+  //     a.remove()
+  //   })
+  //   .catch(err => {
+  //     console.error("err: ", err);
+  //   })
 
-  }
-async function downloadImage(url: string, filename: string) {
-  const response = await fetch(url);
-  const blob = await response.blob();
-  const anchor = document.createElement("a");
-  anchor.download = filename;
-  anchor.href = URL.createObjectURL(blob);
-  document.body.appendChild(anchor);
-  anchor.click();
-  document.body.removeChild(anchor);
 }
 
-  const ImageBot = () => {
-    const router = useRouter()
-    const {id} = router.query;
-    const {data: session} = useSession()
+const upload = async () => {
+  const form = new FormData();
+  const url = "https://oaidalleapiprodscus.blob.core.windows.net/private/org-Gj9qRtTFTcQvSvQMlsTGqRzb/user-WIsWrURgDHs7MJkppHrsBBgZ/img-7DcFcnfPOYn32ihEeiM46Ura.png?st=2023-03-13T17%3A38%3A59Z&se=2023-03-13T19%3A38%3A59Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-03-13T15%3A26%3A21Z&ske=2023-03-14T15%3A26%3A21Z&sks=b&skv=2021-08-06&sig=WkDqWe8k/U4qN6yyYlXPEodYTBD10d/eYp7rwW5VXbw%3D"
+  return fetch(url, {})
+    .then((res) => res.blob())
+    .then((blob) => {
+      const file = new File([blob], 'image', {
+        type: blob.type,
+      });
 
-    const [imageUrl, setImageUrl] = useState<Images[]>([]);
-    const [textLines, setTextLines] = useState(1);
-    const [text, setText] = useState("Two futuristic towers with a skybridge covered in lush foliage, digital art");
-    const [progress, setProgress] = useState(0)
-
-    const generateImageMutate = useMutation(["CHAT_AI"], ({accessToken, text}: ImageAiMutateMutationFn) => {
-      const progressInterval = setInterval(() => {
-        setProgress((prevProgress) => {
-          if (prevProgress === 100) {
-            setProgress(100);
-            clearInterval(progressInterval);
-            return prevProgress;
-          } else {
-            return prevProgress + 1;
-          }
-        });
-      }, 10);
-
-      setTimeout(() => {
-        setProgress(100);
-        clearInterval(progressInterval);
-      }, 5000);
-
-      const aiPromptRequestBody: AIPromptRequestBody = {text: text}
-      return fetch(process.env.NEXT_PUBLIC_AWS_GATEWAY_URL_IMAGEBOT, {
-        method: "POST",
-        headers: {'Authorization': `Bearer ${accessToken}`, "Content-Type": "application/json"},
-        body: JSON.stringify(aiPromptRequestBody),
-      })
-        .then((response) => {
-          if (!response.ok) return Promise.reject(response)
-          return response.json()
-        }).then((value) => {
-          if (value.data) {
-            setProgress(100)
-            setImageUrl(value.data);
-          }
-          return value
-        })
+      form.append('image', file);
+      console.log(form)
+      // axios.post('/upload', form);
     });
+};
 
-    const handleTextareaKeydown = async (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
-      const isCtrlEnterPressed = textLines > 1 && event.ctrlKey && event.key === "Enter";
-      const isEnterPressed = event.key === "Enter";
+const ImageBot = () => {
+  const router = useRouter()
+  const {id} = router.query;
+  const {data: session} = useSession()
 
-      if (isCtrlEnterPressed) {
-        setTextLines(textLines - 1);
-        return;
-      }
+  const [imageUrl, setImageUrl] = useState<Images[]>([]);
+  const [textLines, setTextLines] = useState(1);
+  const [text, setText] = useState("Two futuristic towers with a skybridge covered in lush foliage, digital art");
+  const [progress, setProgress] = useState(0)
 
-      if (isEnterPressed) {
-        event.preventDefault();
-      }
-
-      if (session) {
-        if (isEnterPressed && text !== "") {
-          const chatAiMutateMutationFn: ImageAiMutateMutationFn = {
-            accessToken: session.access_token,
-            text: text,
-          }
-          generateImageMutate.mutate(chatAiMutateMutationFn)
-          setTextLines(1);
-          setText("");
+  const generateImageMutate = useMutation(["CHAT_AI"], ({accessToken, text}: ImageAiMutateMutationFn) => {
+    const progressInterval = setInterval(() => {
+      setProgress((prevProgress) => {
+        if (prevProgress === 100) {
+          setProgress(100);
+          clearInterval(progressInterval);
+          return prevProgress;
+        } else {
+          return prevProgress + 1;
         }
-      }
+      });
+    }, 10);
+
+    setTimeout(() => {
+      setProgress(100);
+      clearInterval(progressInterval);
+    }, 5000);
+
+    const aiPromptRequestBody: AIPromptRequestBody = {text: text}
+    return fetch(process.env.NEXT_PUBLIC_AWS_GATEWAY_URL_IMAGEBOT, {
+      method: "POST",
+      headers: {'Authorization': `Bearer ${accessToken}`, "Content-Type": "application/json"},
+      body: JSON.stringify(aiPromptRequestBody),
+    })
+      .then((response) => {
+        if (!response.ok) return Promise.reject(response)
+        return response.json()
+      }).then((value) => {
+        if (value.data) {
+          setProgress(100)
+          setImageUrl(value.data);
+        }
+        return value
+      })
+  });
+
+  const handleTextareaKeydown = async (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    const isCtrlEnterPressed = textLines > 1 && event.ctrlKey && event.key === "Enter";
+    const isEnterPressed = event.key === "Enter";
+
+    if (isCtrlEnterPressed) {
+      setTextLines(textLines - 1);
+      return;
     }
 
-    return (
-      <div className={"mt-28 flex justify-center"}>
-        <SideMenuImageBot/>
-        <section className={"flex flex-col items-center justify-center "} style={{minHeight: "50vh"}}>
-          <div className={"max-w-screen-xl space-y-5 sm:ml-64 xxs:w-12/12 xs:w-12/12 sm:w-12/12 md:w-8/12"}>
-            <div
-              className={`w-full rounded-full h-2.5 bg-gray-200 bg-zinc-400 dark:bg-zinc-600 ${generateImageMutate.isLoading ? "block" : "hidden"}`}>
-              <div
-                className="bg-gradient-to-t from-gray-300 to-gray-400 dark:bg-orange-1100 dark:from-orange-600 dark:to-amber-900 h-2.5 rounded-full"
-                style={{width: `${progress}%`}}></div>
-            </div>
-            <div
-              className={`grid md:grid-cols-2 lg:grid-cols-4 gap-5`}>
-              {(generateImageMutate.isLoading || imageUrl.length < 3) && (
-                <>
-                  <LoadingImage/>
-                  <LoadingImage/>
-                  <LoadingImage/>
-                  <LoadingImage/>
-                </>
-              )}
-              {(!generateImageMutate.isLoading && imageUrl.length > 3 && progress === 100) && (
-                <>
-                  {imageUrl.map((image, index) => {
-                    return (
-                      <div
-                        key={index}
-                        onClick={() => fetchDownloadMaterial()}
-                      >
-                        <Image
-                          src={image.url}
-                          alt="user_icon"
-                          width={300}
-                          height={300}
-                          className="xxs:h-32 xs:h-32 sm:h-32 md:h-56 lg:h-56 xl:h-56 2xl:h-56 xxs:w-56 xs:w-56 sm:w-56 md:w-64 lg:w-64 xl:w-64 2xl:w-64
-                        cursor-pointer transition duration-100 ease-in-out transform hover:-translate-y-0 hover:scale-110 rounded"
-                          priority={true}
-                        />
-                      </div>
-                    );
-                  })}
-                </>
-              )}
-            </div>
-            <a id="download-link"
-               href="https://oaidalleapiprodscus.blob.core.windows.net/private/org-Gj9qRtTFTcQvSvQMlsTGqRzb/user-WIsWrURgDHs7MJkppHrsBBgZ/img-JBl5E0HKm2Uv8CHwwnz3rT09.png?st=2023-03-13T16%3A33%3A48Z&se=2023-03-13T18%3A33%3A48Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-03-13T08%3A00%3A59Z&ske=2023-03-14T08%3A00%3A59Z&sks=b&skv=2021-08-06&sig=YsjB8sz7KxvTlhBAWF97U0B7RE1XzqMNgzNpe0lx2X4%3D"
-               download="myImage.png" style={{display: "none"}}></a>
-          </div>
-          <button className={"btn"} onClick={() => downloadImage(
-            "https://oaidalleapiprodscus.blob.core.windows.net/private/org-Gj9qRtTFTcQvSvQMlsTGqRzb/user-WIsWrURgDHs7MJkppHrsBBgZ/img-JBl5E0HKm2Uv8CHwwnz3rT09.png?st=2023-03-13T16%3A33%3A48Z&se=2023-03-13T18%3A33%3A48Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-03-13T08%3A00%3A59Z&ske=2023-03-14T08%3A00%3A59Z&sks=b&skv=2021-08-06&sig=YsjB8sz7KxvTlhBAWF97U0B7RE1XzqMNgzNpe0lx2X4%3D",
-            "myimage.png"
-          )}>dawdaw</button>
+    if (isEnterPressed) {
+      event.preventDefault();
+    }
 
-        </section>
-        <footer className="fixed bottom-0 flex w-full justify-center bg-transparent">
-          <form
-            className="mb-2 w-full xxs:w-11/12 sm:ml-64 sm:w-6/12 sm:w-7/12 md:w-7/12 md:w-10/12 lg:w-8/12 lg:w-11/12 xl:w-9/12"
-            onSubmit={(e) => e.preventDefault()}
-          >
-            <div className="relative w-full">
+    if (session) {
+      if (isEnterPressed && text !== "") {
+        const chatAiMutateMutationFn: ImageAiMutateMutationFn = {
+          accessToken: session.access_token,
+          text: text,
+        }
+        generateImageMutate.mutate(chatAiMutateMutationFn)
+        setTextLines(1);
+        setText("");
+      }
+    }
+  }
+
+  return (
+    <div className={"mt-28 flex justify-center"}>
+      <SideMenuImageBot/>
+      <section className={"flex flex-col items-center justify-center "} style={{minHeight: "50vh"}}>
+        <div className={"max-w-screen-xl space-y-5 sm:ml-64 xxs:w-12/12 xs:w-12/12 sm:w-12/12 md:w-8/12"}>
+          <div
+            className={`w-full rounded-full h-2.5 bg-gray-200 bg-zinc-400 dark:bg-zinc-600 ${generateImageMutate.isLoading ? "block" : "hidden"}`}>
+            <div
+              className="bg-gradient-to-t from-gray-300 to-gray-400 dark:bg-orange-1100 dark:from-orange-600 dark:to-amber-900 h-2.5 rounded-full"
+              style={{width: `${progress}%`}}></div>
+          </div>
+          <div
+            className={`grid md:grid-cols-2 lg:grid-cols-4 gap-5`}>
+            {(generateImageMutate.isLoading || imageUrl.length < 3) && (
+              <>
+                <LoadingImage/>
+                <LoadingImage/>
+                <LoadingImage/>
+                <LoadingImage/>
+              </>
+            )}
+            {(!generateImageMutate.isLoading && imageUrl.length > 3 && progress === 100) && (
+              <>
+                {imageUrl.map((image, index) => {
+                  return (
+                    <div
+                      key={index}
+                      onClick={() => fetchDownloadMaterial(image.url)}
+                    >
+                      <Image
+                        src={image.url}
+                        alt="user_icon"
+                        width={300}
+                        height={300}
+                        className="xxs:h-32 xs:h-32 sm:h-32 md:h-56 lg:h-56 xl:h-56 2xl:h-56 xxs:w-56 xs:w-56 sm:w-56 md:w-64 lg:w-64 xl:w-64 2xl:w-64
+                        cursor-pointer transition duration-100 ease-in-out transform hover:-translate-y-0 hover:scale-110 rounded"
+                        priority={true}
+                      />
+                    </div>
+                  );
+                })}
+              </>
+            )}
+          </div>
+        </div>
+        <button className={"btn"} onClick={() => upload()}>Download</button>
+      </section>
+      <footer className="fixed bottom-0 flex w-full justify-center bg-transparent">
+        <form
+          className="mb-2 w-full xxs:w-11/12 sm:ml-64 sm:w-6/12 sm:w-7/12 md:w-7/12 md:w-10/12 lg:w-8/12 lg:w-11/12 xl:w-9/12"
+          onSubmit={(e) => e.preventDefault()}
+        >
+          <div className="relative w-full">
               <textarea
                 disabled={generateImageMutate.isLoading}
                 className="textarea-bordered textarea w-full resize-none rounded-2xl bg-zinc-300
@@ -210,24 +209,24 @@ async function downloadImage(url: string, filename: string) {
                 onKeyDown={handleTextareaKeydown}
                 value={text}
               />
+            <div
+              className={`absolute inset-y-0 right-0 bottom-1 flex items-center pr-3 ${generateImageMutate.isLoading ? "block" : "hidden"} opacity-50`}>
               <div
-                className={`absolute inset-y-0 right-0 bottom-1 flex items-center pr-3 ${generateImageMutate.isLoading ? "block" : "hidden"} opacity-50`}>
-                <div
-                  className="inline-block h-8 w-8 animate-[spinner-grow_0.75s_linear_infinite]
+                className="inline-block h-8 w-8 animate-[spinner-grow_0.75s_linear_infinite]
                 rounded-full bg-current align-[-0.125em] opacity-0 motion-reduce:animate-[spinner-grow_1.5s_linear_infinite]
                 bg-gradient-to-t from-gray-400 to-gray-300 text-gray-900 opacity-0 dark:bg-orange-1100 dark:from-orange-600 dark:to-amber-900"
-                  role="status">
+                role="status">
                 <span
                   className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
                   Loading...
                 </span>
-                </div>
               </div>
             </div>
-          </form>
-        </footer>
-      </div>
-    )
-  };
+          </div>
+        </form>
+      </footer>
+    </div>
+  )
+};
 
-  export default ImageBot;
+export default ImageBot;

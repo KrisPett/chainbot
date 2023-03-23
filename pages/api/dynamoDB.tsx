3400197551dde1fbf616d21f8760195463f8ce24
KQT3 <70701718+KQT3@users.noreply.github.com>
2023-03-23 19:27:28 +0100

    commit

--- pages/api/dynamoDB.tsx
+++ pages/api/dynamoDB.tsx
@@ -13,58 +13,16 @@ const dynamodb = new AWS.DynamoDB({
 });
 
 export const images = {
-    "M": {
-      "images": {
-        "L": [
-          {
-            "M": {
-              "imageId": {
-                "S": "cb9a75a2-2553-4c9f-ab0a-1388c54d6621"
-              },
-              "url": {
-                "S": "https://s3.amazonaws.com/chainbot.chaincuet.com.storage/imagebot/cb9a75a2-2553-4c9f-ab0a-1388c54d6621"
-              }
-            }
-          },
-          {
-            "M": {
-              "imageId": {
-                "S": "09ed4bf2-e43c-4a25-804b-5f55bc94090d"
-              },
-              "url": {
-                "S": "https://s3.amazonaws.com/chainbot.chaincuet.com.storage/imagebot/09ed4bf2-e43c-4a25-804b-5f55bc94090d"
-              }
-            }
-          },
-          {
-            "M": {
-              "imageId": {
-                "S": "a49da393-db67-4886-951c-a9a63b66348b"
-              },
-              "url": {
-                "S": "https://s3.amazonaws.com/chainbot.chaincuet.com.storage/imagebot/a49da393-db67-4886-951c-a9a63b66348b"
-              }
-            }
-          },
-          {
-            "M": {
-              "imageId": {
-                "S": "f0116257-4c8d-4edb-b48e-cbe500a67d6a"
-              },
-              "url": {
-                "S": "https://s3.amazonaws.com/chainbot.chaincuet.com.storage/imagebot/f0116257-4c8d-4edb-b48e-cbe500a67d6a"
-              }
-            }
-          }
-        ]
-      },
-      "imagesCollectionId": {
-        "S": "5ec5d9a2-3104-4472-89ad-fc45bf4ade51"
-      },
-      "timestamp": {
-        "S": "2022-03-21T10:30:00Z"
-      }
-    }
+    "images": {
+      "L": [
+        {"M": {"imageId": {"S": "cb9a75a2-2553-4c9f-ab0a-1388c54d6621"}, "url": {"S": "https://s3.amazonaws.com/chainbot.chaincuet.com.storage/imagebot/cb9a75a2-2553-4c9f-ab0a-1388c54d6621"}}},
+        {"M": {"imageId": {"S": "09ed4bf2-e43c-4a25-804b-5f55bc94090d"}, "url": {"S": "https://s3.amazonaws.com/chainbot.chaincuet.com.storage/imagebot/09ed4bf2-e43c-4a25-804b-5f55bc94090d"}}},
+        {"M": {"imageId": {"S": "a49da393-db67-4886-951c-a9a63b66348b"}, "url": {"S": "https://s3.amazonaws.com/chainbot.chaincuet.com.storage/imagebot/a49da393-db67-4886-951c-a9a63b66348b"}}},
+        {"M": {"imageId": {"S": "f0116257-4c8d-4edb-b48e-cbe500a67d6a"}, "url": {"S": "https://s3.amazonaws.com/chainbot.chaincuet.com.storage/imagebot/f0116257-4c8d-4edb-b48e-cbe500a67d6a"}}}
+      ]
+    },
+    "imagesCollectionId": {"S": "5ec5d9a2-3104-4472-89ad-fc45bf4ade51"},
+    "timestamp": {"S": "2022-03-21T10:30:00Z"}
 }
 
 
@@ -77,10 +35,8 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       TableName: "user-images-test",
       Key: {"userId": {"S": subId}},
       UpdateExpression: "SET #images = list_append(#images, :new_images)",
-      ExpressionAttributeNames: {"#images": "imagesCollection"},
-      ExpressionAttributeValues: {
-        ":new_images": {"M": images.M}
-      },
+      ExpressionAttributeNames: { "#images": "imagesCollection" },
+      ExpressionAttributeValues: { ":new_images": { "L": [ { "M": images } ] } },
     }
 
     try {

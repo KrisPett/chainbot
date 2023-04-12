270e68342ea278d319c2a026e231b09d1c989f18
KQT3 <70701718+KQT3@users.noreply.github.com>
2023-04-12 20:07:16 +0200

    commit

--- components/home/CreditCard.tsx
+++ components/home/CreditCard.tsx
@@ -6,25 +6,25 @@ const CreditCard = () => {
   return (
     <div>
       <div className="bg-transparent min-h-screen flex justify-center items-center">
-        <div className="space-y-16">
+        <div className="space-y-16 ">
           <div className="xxs:w-56 xxs:h-36 xs:w-64 xs:h-50 sm:w-96 sm:h-56
-          m-auto bg-red-700 rounded-xl relative text-white shadow-2xl transition-transform transform hover:scale-110
-          group-hover:to-orange-400 bg-gradient-to-b from-zinc-300 to-green-200 dark:bg-transparen dark:from-transparent
+          m-auto rounded-xl relative text-white shadow-2xl transition-transform transform hover:scale-110
+          group-hover:to-orange-400 bg-gradient-to-b from-zinc-300 to-zinc-200 dark:bg-orange-1000 dark:from-transparent
           dark:to-orange-1100 dark:text-gray-300 border-amber-900 dark:hover:text-white dark:group-hover:to-orange-400
           ">
-            <div className="w-full px-8 absolute top-8">
+            <div className="w-full px-8 absolute top-8 ">
               <div className="flex justify-between">
                 <div className="">
-                  <p className="font-light">Name</p>
-                  <p className="font-medium tracking-widest">Chaincue Technologies</p>
+                  <p className="font-light dark:text-gray-300 text-gray-900 dark:hover:text-white">Name</p>
+                  <p className="font-medium tracking-widest dark:text-gray-300 text-gray-900 dark:hover:text-white">Chaincue Technologies</p>
                 </div>
                 <Image className="w-8 h-14" src={ethereum_logo} alt={"alt"} width={100} height={100}/>
               </div>
-              <div className="pt-20">
+              <div className="xxs:pt-0 sm:pt-20">
                 <div className="flex justify-between">
                   <div className="">
-                    <p className="font-light text-xs">Address</p>
-                    <p className="font-medium tracking-wider text-sm">Ethereum</p>
+                    <p className="font-light text-xs dark:text-gray-300 text-gray-900 dark:hover:text-white">Address</p>
+                    <p className="font-medium tracking-wider text-sm dark:text-gray-300 text-gray-900 dark:hover:text-white">Ethereum</p>
                   </div>
                 </div>
               </div>
--- components/home/HomeView.tsx
+++ components/home/HomeView.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import CreditCard from "@/components/home/CreditCard";
 
 const HomeView = () => {
+  const address = "0x000000";
   return (
     <main className={"xxs:mt-10 md:mt-44"}>
       <div className={"flex xxs:flex-col lg:flex-row justify-center xl:gap-x-64"}>
@@ -9,7 +10,8 @@ const HomeView = () => {
           <div className={"flex flex-col items-center"}>
             <p className={"text-5xl max-w-2xl w-11/12"}>Experience the Future of Art and Conversation with OpenAI&apos;s
               Technology</p>
-            <p className={"text-lg max-w-2xl mt-10 w-11/12"}>Discover the power of Chaincue by generating unique images using
+            <p className={"text-lg max-w-2xl mt-10 w-11/12"}>Discover the power of Chaincue by generating unique images
+              using
               OpenAI
               DALL-E image generator and then
               minting them on the Ethereum blockchain.</p>
@@ -17,8 +19,8 @@ const HomeView = () => {
           <div className={"w-full mt-20 flex justify-center"}>
             <button onClick={() => console.log("dwdws")} className="
             group btn relative text-xl text-gray-800 hover:text-gray-700 group-hover:to-orange-400 bg-gradient-to-b
-            from-zinc-300 to-green-200 dark:bg-transparen dark:from-transparent dark:to-orange-1100 dark:text-gray-300
-            border-amber-900 dark:hover:text-white dark:group-hover:to-orange-400 normal-case inline-flex justify-center
+            from-zinc-300 to-zinc-200 dark:bg-transparen dark:from-transparent dark:to-orange-1100 dark:text-gray-300
+            border-zinc-300 dark:border-amber-900 dark:hover:text-white dark:group-hover:to-orange-400 normal-case inline-flex justify-center
             px-3 py-2 font-semibold shadow-sm hover:bg-opacity-0 xxs:w-11/12 sm:w-11/12">
               Connect Wallet
             </button>
@@ -28,10 +30,12 @@ const HomeView = () => {
           <div className={"flex flex-col items-center gap-4 mt-10"}>
             <CreditCard/>
             <div className="w-full xxs:w-11/12 rounded-2xl mt-12">
-            <span className="bg-gray-200 bg-gradient-to-t from-gray-200 to-gray-300 text-gray-900
-            dark:bg-orange-1100 dark:from-orange-600 dark:to-amber-900 dark:text-gray-300 rounded-2xl "></span>
-              <div className="grid w-full h-20 bg-zinc-800 place-items-center rounded-2xl">
-                Address to receive NFTs...
+              <span className="rounded-2xl"></span>
+              <div data-tip="copy" onClick={() =>  navigator.clipboard.writeText(address)}
+                   className="tooltip cursor-pointer grid w-full h-20 place-items-center rounded-2xl group-hover:to-orange-400 bg-gradient-to-b
+            from-zinc-300 to-zinc-300 dark:bg-transparen dark:from-orange-900 dark:to-orange-1100 dark:text-gray-300
+            border-amber-900 dark:hover:text-white dark:group-hover:to-orange-400 active:text-sm">
+                {address + "..." + address.slice(-4)}
               </div>
             </div>
           </div>

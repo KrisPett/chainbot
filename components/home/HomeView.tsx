6d08ef0bd1882b9f75cc3e8e8e385b16956b0828
KQT3 <70701718+KQT3@users.noreply.github.com>
2023-04-13 20:39:59 +0200

    commit

--- components/home/HomeView.tsx
+++ components/home/HomeView.tsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import CreditCard from "@/components/home/CreditCard";
 
+const isMetamaskInstalled = typeof window !== 'undefined' && typeof window.ethereum !== 'undefined';
+
 const HomeView = () => {
-  const address = "0x000000";
+  const [isLoadingConfirm, setIsLoadingConfirm] = useState<boolean>(false);
+  const [isMetamaskConnected, setIsMetamaskConnected] = useState<boolean>(false);
+  const [ethAddress, setEthAddress] = useState<string>("0x000000");
+
+  useEffect(() => {
+    if (isMetamaskInstalled) {
+      if (window.ethereum.selectedAddress) {
+        setIsMetamaskConnected(true)
+        setEthAddress(window.ethereum.selectedAddress)
+      }
+    }
+  }, [isLoadingConfirm]);
+
+  const connectWallet = async () => {
+    setIsLoadingConfirm(true)
+    const accounts = await window.ethereum.request({method: 'eth_requestAccounts'})
+      .then((accounts) => {
+        console.log(accounts)
+        setIsLoadingConfirm(false)
+      }).catch((err) => {
+        console.log(err)
+        setIsLoadingConfirm(false)
+      });
+  }
+
   return (
     <main className={"xxs:mt-10 md:mt-44"}>
       <div className={"flex xxs:flex-col lg:flex-row justify-center xl:gap-x-64"}>
@@ -17,12 +43,12 @@ const HomeView = () => {
               minting them on the Ethereum blockchain.</p>
           </div>
           <div className={"w-full mt-20 flex justify-center"}>
-            <button onClick={() => console.log("dwdws")} className="
+            <button disabled={isMetamaskConnected || !isMetamaskInstalled} onClick={() => connectWallet()} className="disabled:opacity-70
             group btn relative text-xl text-gray-800 hover:text-gray-700 group-hover:to-orange-400 bg-gradient-to-b
             from-zinc-300 to-zinc-200 dark:bg-transparen dark:from-orange-1000 dark:to-orange-1100 dark:text-gray-300
-            border-zinc-300 border-1 border-base-300/10 hover:dark:border-orange-1100 dark:hover:text-white dark:group-hover:to-orange-400 normal-case inline-flex justify-center
+            border-zinc-300 border-1 border-base-300/20 hover:dark:border-orange-1100 dark:hover:text-white dark:group-hover:to-orange-400 normal-case inline-flex justify-center
             px-3 py-2 font-semibold shadow-sm hover:bg-opacity-0 xxs:w-11/12 sm:w-11/12">
-              Connect Wallet
+              {!isMetamaskInstalled ? "Metamask Required" : isMetamaskConnected ? "You Are Connected" : isLoadingConfirm ? "Pending Metamask Extension..." : "Connect Wallet"}
             </button>
           </div>
         </section>
@@ -31,11 +57,11 @@ const HomeView = () => {
             <CreditCard/>
             <div className="w-full xxs:w-11/12 rounded-2xl mt-12">
               <span className="rounded-2xl"></span>
-              <div data-tip="copy" onClick={() =>  navigator.clipboard.writeText(address)}
+              <div data-tip="copy" onClick={() => navigator.clipboard.writeText(ethAddress)}
                    className="tooltip cursor-pointer grid w-full h-20 place-items-center rounded-2xl group-hover:to-orange-400 bg-gradient-to-b
             from-zinc-300 to-zinc-300 dark:bg-transparen dark:from-orange-900 dark:to-orange-1100 dark:text-gray-300
             border-amber-900 dark:hover:text-white dark:group-hover:to-orange-400 active:brightness-110">
-                {address + "..." + address.slice(-4)}
+                {ethAddress.slice(0, 20) + "..." + ethAddress.slice(-4)}
               </div>
             </div>
           </div>

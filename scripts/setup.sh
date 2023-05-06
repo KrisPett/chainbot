472b1e739a7e581bbadf37fb3caa61c2b9c4b986
KQT3 <70701718+KQT3@users.noreply.github.com>
2023-05-06 11:10:24 +0300

    commit

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 
-# create a new .env file or overwrite an existing one
-touch .env
-
 # add the required environment variables to the .env file
-echo "NEXT_PUBLIC_CLIENT_URL=" >> .env
-echo "NEXT_PUBLIC_AWS_GATEWAY_URL_CHATBOT=" >> .env
-echo "NEXT_PUBLIC_AWS_GATEWAY_URL_IMAGEBOT=" >> .env
-echo "NEXT_PUBLIC_AWS_GATEWAY_URL_IMAGEBOT_FILTER_IMAGES=" >> .env
-echo "NEXT_PUBLIC_WALLET_CONNECT_ID=" >> .env
-echo "NEXTAUTH_URL=" >> .env
-echo "NEXTAUTH_SECRET=" >> .env
-echo "KEYCLOAK_ID=" >> .env
-echo "KEYCLOAK_SECRET=" >> .env
-echo "KEYCLOAK_ISSUER=" >> .env
+echo "NEXT_PUBLIC_CLIENT_URL=" >> .env.local
+echo "NEXT_PUBLIC_AWS_GATEWAY_URL_CHATBOT=" >> .env.local
+echo "NEXT_PUBLIC_AWS_GATEWAY_URL_IMAGEBOT=" >> .env.local
+echo "NEXT_PUBLIC_AWS_GATEWAY_URL_IMAGEBOT_FILTER_IMAGES=" >> .env.local
+echo "NEXT_PUBLIC_WALLET_CONNECT_ID=" >> .env.local
+echo "NEXTAUTH_URL=" >> .env.local
+echo "NEXTAUTH_SECRET=" >> .env.local
+echo "KEYCLOAK_ID=" >> .env.local
+echo "KEYCLOAK_SECRET=" >> .env.local
+echo "KEYCLOAK_ISSUER=" >> .env.local
